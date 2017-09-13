f9c97335f223f0cc6ab0d451002e3d9148985d3d
Alberto Gaona <albertoo_3c@hotmail.com>
2017-09-13 18:39:01 -0500

    define get_legal_moves

--- isolation.py
+++ isolation.py
@@ -44,9 +44,26 @@ class GameState:
         be a pair of integers in (column, row) order specifying
         the zero-indexed coordinates on the board.
         """
-        pass
+        location = self.player_locations[self.player]
+        if not location:
+        	return self.get_blank_spaces()
+
+       	moves = []
+       	rays = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+       	for dx, dy in rays:
+       		_x, _y = location
+       		while 0 <= _x + dx < xlimit and 0 <= _y + dy < ylimit:
+       			_x, _y = _x +dx, _y + dy
+       			if self.board[_x][_y]:
+       				break
+       			moves.append((_x,_y))
+       	return moves
+
+
 
     def get_blank_spaces(self):
+    	""" Return a list of blank spaces on the board. """
     	return [(x,y) for y in range(ylimit) for x in range(xlimit) if self.board[x][y] == 0]
 
 class testcode:
@@ -54,16 +71,14 @@ class testcode:
 	print("Creating empty game board...")
 	g = GameState()
 
-	print(g.board)
-
-	print(g.get_blank_spaces())
-
-	"""print("Getting legal moves for player 1...")
+	print("Getting legal moves for player 1...")
 	p1_empty_moves = g.get_legal_moves()
 	print("Found {} legal moves.".format(len(p1_empty_moves or [])))
 
 	print("Applying move (0, 0) for player 1...")
 	g1 = g.forecast_move((0, 0))
+
+	print(g1.board)
 
 	print("Getting legal moves for player 2...")
 	p2_empty_moves = g1.get_legal_moves()
@@ -71,4 +86,4 @@ class testcode:
 	    print("Failed\n  Uh oh! (0, 0) was not blocked properly when " +
 	          "player 1 moved there.")
 	else:
-	    print("Everything looks good!")"""
+	    print("Everything looks good!")
