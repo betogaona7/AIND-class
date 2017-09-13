85267563a7a978c74c42b9565fc8d57362e121f5
Alberto Gaona <albertoo_3c@hotmail.com>
2017-09-12 20:46:31 -0500

    Isolation game - create class GameState and define forecast move function

---
+++ isolation.py
@@ -0,0 +1,74 @@
+"""
+	Here we are implement the minimax algorithm to solve the isolation game.
+"""
+from copy import deepcopy
+
+xlimit, ylimit = 3, 2 # Board dimensions
+
+class GameState:
+
+    def __init__(self):
+    	self.board = [[0] * ylimit for _ in range(xlimit)] # Create the board 2*3  [0 0 0]
+    													   #					   [0 0 0]
+    	self.board[2][1] = 1 # Block the lower rigth corner [0 0 0]
+    						 #                              [0 0 1]
+    	self.player = 0 # Active player; 0 indicates that player one has initiative and 1 indicates player 2
+    	self.player_locations = [None, None] # Keep track of the current location of each player on the board
+    	
+    
+    def forecast_move(self, move):
+        """ Return a new board object with the specified move
+        applied to the current game state.
+        
+        Parameters
+        ----------
+        move: tuple
+            The target position for the active player's next move
+        """
+        if move not in self.get_legal_moves():
+        	return "Attempted forecast of illegal move"
+        newBoard = deepcopy(self)
+        newBoard.board[move[0]][move[1]] = 1
+        newBoard.player_locations[self.player] = move
+        newBoard.player ^= 1
+        return newBoard
+    
+    def get_legal_moves(self):
+        """ Return a list of all legal moves available to the
+        active player.  Each player should get a list of all
+        empty spaces on the board on their first move, and
+        otherwise they should get a list of all open spaces
+        in a straight line along any row, column or diagonal
+        from their current position. (Players CANNOT move
+        through obstacles or blocked squares.) Moves should
+        be a pair of integers in (column, row) order specifying
+        the zero-indexed coordinates on the board.
+        """
+        pass
+
+    def get_blank_spaces(self):
+    	return [(x,y) for y in range(ylimit) for x in range(xlimit) if self.board[x][y] == 0]
+
+class testcode:
+
+	print("Creating empty game board...")
+	g = GameState()
+
+	print(g.board)
+
+	print(g.get_blank_spaces())
+
+	"""print("Getting legal moves for player 1...")
+	p1_empty_moves = g.get_legal_moves()
+	print("Found {} legal moves.".format(len(p1_empty_moves or [])))
+
+	print("Applying move (0, 0) for player 1...")
+	g1 = g.forecast_move((0, 0))
+
+	print("Getting legal moves for player 2...")
+	p2_empty_moves = g1.get_legal_moves()
+	if (0, 0) in set(p2_empty_moves):
+	    print("Failed\n  Uh oh! (0, 0) was not blocked properly when " +
+	          "player 1 moved there.")
+	else:
+	    print("Everything looks good!")"""
