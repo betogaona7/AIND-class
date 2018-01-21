c3af66f94ebf26a37c5620c90c08f96ba97bd578
Alberto Gaona <albertoo_3c@hotmail.com>
2018-01-21 16:20:04 -0600

    create a cnn to classify images from the CIFAR-10 dataset

---
+++ CNN-cifar10/cnn_cifar.py
@@ -0,0 +1,53 @@
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+
+# Load the pre-shuffled train and test data
+(x_train, y_train),(x_test, y_test) = cifar10.load_data()
+
+# Rescale the images 
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+
+# One hot encode the labes
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+# break training set into training and validation sets
+# 45000 train samples, 10000 test samples, 5000 validation samples
+(x_train, x_validation) = x_train[5000:], x_train[:5000]
+(y_train, y_validation) = y_train[5000:], y_train[:5000]
+
+# Define the model architecture
+model = Sequential()
+
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(32,32,3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(10, activation='softmax'))
+
+# model.summary()
+# Compile the model 
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+# Train the model
+checkpointer = ModelCheckpoint(filepath='./cnncifar.weights.best.hdf5', verbose=1, save_best_only=True)
+hist = model.fit(x_train, y_train, batch_size=32, epochs=100, validation_data=(x_validation, y_validation),
+				 callbacks=[checkpointer], verbose=2, shuffle=True)
+
+# Load the weights that yielded the best validation accuracy
+model.load_weights('./cnncifar.weights.best.hdf5')
+
+# Evaluate and print test accuracy
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test accuracy: ', score[1])
+
