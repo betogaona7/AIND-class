792d6296f2e2068ca0f0c9fbcd9203415d44825a
Alberto Gaona <albertoo_3c@hotmail.com>
2018-01-22 00:27:56 +0000

    improve the network

--- CNN-cifar10/cnn_cifar.py
+++ CNN-cifar10/cnn_cifar.py
@@ -2,7 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from keras.datasets import cifar10
 from keras.utils import np_utils
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 # Load the pre-shuffled train and test data
 (x_train, y_train),(x_test, y_test) = cifar10.load_data()
@@ -23,16 +23,19 @@ y_test = np_utils.to_categorical(y_test, 10)
 # Define the model architecture
 model = Sequential()
 
-model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(32,32,3)))
+model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(32,32,3)))
+model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.3))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
-model.add(Dense(500, activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.25))
 model.add(Dense(10, activation='softmax'))
 
 # model.summary()
@@ -41,8 +44,10 @@ model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['ac
 
 # Train the model
 checkpointer = ModelCheckpoint(filepath='./cnncifar.weights.best.hdf5', verbose=1, save_best_only=True)
+earlystop = EarlyStopping(patience=10)
+
 hist = model.fit(x_train, y_train, batch_size=32, epochs=100, validation_data=(x_validation, y_validation),
-				 callbacks=[checkpointer], verbose=2, shuffle=True)
+				 callbacks=[checkpointer, earlystop], verbose=2, shuffle=True)
 
 # Load the weights that yielded the best validation accuracy
 model.load_weights('./cnncifar.weights.best.hdf5')
