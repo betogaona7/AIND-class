ebe9d63fe2a1c2e7ee36ecfd3a14b5b5390218f7
Alberto Gaona <albertoo_3c@hotmail.com>
2018-01-31 17:34:46 -0600

    First pass an image through a small low pass filter to get ride of noise and then apply high pass filter for edge detection

--- Computer-Vision/filter.py
+++ Computer-Vision/filter.py
@@ -12,6 +12,10 @@ image_copy = cv2.cvtColor(image_copy, cv2.COLOR_BGR2RGB)
 # Convert to grayscale for filtering
 gray = cv2.cvtColor(image_copy, cv2.COLOR_RGB2GRAY)
 
+
+# Implement a low-pass filter first
+gray_blur = cv2.GaussianBlur(gray, (5,5), 0)
+
 # Create a custom kernel 3x3 to detect vertical edges and ignore 
 # horizontal edges
 kernel = np.array([[-1, 0, 1],
@@ -20,7 +24,7 @@ kernel = np.array([[-1, 0, 1],
 
 # Perfom convolution using filter2D which has inputs: (grayscale
 # image, bit-depth, kernel)
-filtered_image = cv2.filter2D(gray, -1, kernel)
+filtered_image = cv2.filter2D(gray_blur, -1, kernel)
 #plt.imshow(filtered_image, cmap='gray')
 
 # Create threshold that sets all the filtered pixels to white 
@@ -28,3 +32,5 @@ filtered_image = cv2.filter2D(gray, -1, kernel)
 
 retval, binary_image = cv2.threshold(filtered_image, 100, 255, cv2.THRESH_BINARY)
 plt.imshow(filtered_image, cmap='gray')
+
+
