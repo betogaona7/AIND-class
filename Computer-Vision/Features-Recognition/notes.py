1a273e6b574e8525eef3c96ede46ee3a01b7ee66
Alberto Gaona <albertoo_3c@hotmail.com>
2018-02-05 19:12:41 -0600

    add notes about haar cascades

--- Computer-Vision/Features-Recognition/notes.py
+++ Computer-Vision/Features-Recognition/notes.py
@@ -76,3 +76,21 @@ result = svm.predict_all(test_data)
 mask = result==labels
 correct = np.count_nonzero(mask)
 print (correct*100.0/result.size)
+
+"""----- HAAR CASCADES -----"""
+
+import numpy as np
+import cv2
+
+# Load in the face detector XML file
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read in an image
+image = cv2.imread('face1.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+#Convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect the faces in the image
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
