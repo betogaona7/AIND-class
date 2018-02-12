9362a67fe4e8aa09caa3cfff56e625ef9bb430f7
Alberto Gaona <albertoo_3c@hotmail.com>
2018-02-12 17:07:22 -0600

    add a netowrk to get a function that predicts odd numbers - network without data to train

---
+++ oddnumbersnet.py
@@ -0,0 +1,21 @@
+from keras.models import Sequential
+from keras.layers import Dense
+"""
+Suposse we had the sequence [1, 3, 5, 7, 9, 11, 13, 15] and want to find a 
+function that generates it. 
+"""
+
+model = Sequential()
+
+# g(s) = W0 + W1S the network output should be approx f(s) = 2+1S
+
+# Fully connected layer
+layer = Dense(1, input_dim=1, activation='linear')
+model.add(layer)
+
+# Compile and train the network
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x, y, epochs=3000, batch_size=3, callbacks=[], verbose=0)
+
+# Print learned weights 
+print(model.get_weights())
