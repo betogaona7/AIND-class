b8ff26c7f1785c187016eb187c3778fe306d7399
Alberto Gaona <albertoo_3c@hotmail.com>
2018-03-09 13:04:33 -0600

    expand utterances and facts

--- AIND-VUI-Alexa/src/facts.js
+++ AIND-VUI-Alexa/src/facts.js
@@ -7,6 +7,15 @@ module.exports = {
         "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
         "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
         "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "In the 1950 Alan Turing invented a test (aptly called the Turing Test) to prove if a machine exhibits artificial Intelligence.",
+        "In January 2015, Stephen Hawking, Elon Musk, and dozens of Artificial Intelligence experts signed an open letter on AI, calling for research on the societal impacts and urging researchers not to create something which cannot be controlled.",
+        "Billionaire Dmitry Itskov created a non-profit organization called the 2045 Initiative, where they want to make people immortal by adding human intelligence into artificial bodies by the year 2045.",
+        "A scientist named Carl Djerassi not only developed the birth control pill, but also wrote over 1200 papers on different subjects, and wrote one of the first Artificial Intelligence programs in 1965.",
+        "As far back as 2006 NASA was using Artificial Intelligence to perform evolutionary processes that created an optimally designed space antenna after millions of iterations in only 10 hours.",
+        "The Mechanical Turk was an automaton chess machine constructed in 1770 that defeated both Benjamin Franklin and Napoleon. In reality it turned out to be an elaborate hoax, still, it lasted 50 years, and people believed that this historic “robot” demonstrated Artificial Intelligence.",
+        "In 1966 a group of M.I.T. students used a computer in the Artificial Intelligence lab to determine the fastest route through the entire New York City subway system, which they took during their Spring Break.",
+        "AI pets exist: These AI powered pets remove the downsides of having a pet e.g. have to clean up after them. Few AI driven pets exist but they are thought to become widely available by 2025.",
+        "As early as by the end of 2018, it is thought that robotic assistants will be able to carry out facial recognition as well as voice recognition.",
+        " In 2013, the most advanced AI had a four-year child's mind, but in 2014 another AI was able to solve mathematical problems of high level.",
     ]
 }
